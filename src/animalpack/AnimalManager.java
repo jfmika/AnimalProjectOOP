65da8d14be03a72cfeff88a79ddd994022335903
Joy-Farai Mika <J.Mika@sacschool.com>
2019-05-16 08:04:35 +0200

    added AnimalManager class to read in data from data.txt file

---
+++ src/animalpack/AnimalManager.java
@@ -0,0 +1,40 @@
+package animalpack;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class AnimalManager {
+    private Animal[] allAnimals = new Animal[25];
+    private int aCounter = 0;
+
+    public AnimalManager() {
+        try {
+            Scanner f = new Scanner(new File("animal_data.txt"));
+            Animal cur = null;
+            while (f.hasNextLine()) {
+                Scanner line = new Scanner(f.nextLine()).useDelimiter(",");
+                if (line.next().equals("A")) {
+                    // found animal
+                    cur = new Animal(line.next(), line.nextInt());
+                } else {
+                    // found a bird
+                    cur = new Bird(line.next(), line.nextInt()
+                            , line.next(), line.nextInt());
+                }
+                // more coming here
+                allAnimals[aCounter] = cur;
+                aCounter++;
+            }
+        } catch (FileNotFoundException e) {
+            System.err.println("File not found");
+        }
+    }
+
+    // let's display the contents of the array
+    public void printAll() {
+        for (int i = 0; i < aCounter; i++) {
+            System.out.println(allAnimals[i].toString());
+        }
+    }
+}
