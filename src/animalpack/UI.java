11b94c3e9e2a313c6ce41d6ec2bd38a88d04f4c8
Joy-Farai Mika <J.Mika@sacschool.com>
2019-05-17 13:49:42 +0200

    cleaned up sorting code

--- src/animalpack/UI.java
+++ src/animalpack/UI.java
@@ -1,8 +1,5 @@
 package animalpack;
 
-import java.lang.reflect.Array;
-import java.util.ArrayList;
-
 public class UI {
     public static void main(String[] args) {
         // testing the AnimalManager class
@@ -12,43 +9,10 @@ public class UI {
         aMan.sortAnimalsBySpeed();
         aMan.printAll();
 
-//        // testing how to sort
-//        int[] nums = new int[25];
-//        for (int i = 0; i < nums.length; i++){
-//            nums[i] = (int)(Math.random() * 90) + 10;
-//        }
-//        for (int i = 0; i < nums.length; i++){
-//            System.out.print(nums[i]);
-//            if(i < nums.length - 1){
-//                System.out.print(",");
-//            }
-//        }
-//        System.out.println();
-//        int[] temp = ;
-//        // call sort
-//        sort(nums);
-//        for (int i = 0; i < nums.length; i++){
-//            System.out.print(nums[i]);
-//            if(i < nums.length - 1){
-//                System.out.print(",");
-//            }
-//        }
-//
-//        sortE(temp);
-//
-//        for (int i = 0; i < temp.length; i++){
-//            System.out.print(temp[i]);
-//            if(i < temp.length - 1){
-//                System.out.print(",");
-//            }
-//        }
-
-    }
 
     public static void sort(int[] myNums) {
         int count = 0;
-        for (int i = 0; i < myNums.length; i++) {
-            boolean madeSwap = false;
+            for (int i = 0; i < myNums.length; i++) {
             for (int j = 1; j < myNums.length - i; j++) {
                 if (myNums[j - 1] > myNums[j]) {
                     int temp = myNums[j - 1];
@@ -58,9 +22,6 @@ public class UI {
                     count++;
                 }
             }
-//            if(!madeSwap){
-//                break;
-//            }
         }
         System.out.println("[" + count + "]");
 
